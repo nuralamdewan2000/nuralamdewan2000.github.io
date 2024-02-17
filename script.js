4e89b8d14f1e458e401ed5b0fb91e7b1ac83873b
Nuralam <nuralam.dewan@zapuza.com>
2024-02-17 15:09:02 +0530

    protfolio

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+        // if(this.scroll>500){
+        //     $('.scroll-up-btn').addClass("show");
+        // }
+        // else{
+        //     $('.scroll-up-btn').removeClass("show");
+        // }
+    })
+//slide-up script
+//    $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop:0});
+//   });
+
+
+    // toogle menu/navbar script
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i ').toggleClass("active")
+    })
+})
+
+    // typing Animation  script
+
+    var typed = new Typed(".typing",{
+        strings: ["Developer","Designer","Creator"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    })
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Developer","Designer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    })
+
+    var typed = new Typed(".typing-3",{
+        strings: ["Developer...","Designer..."],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    })
